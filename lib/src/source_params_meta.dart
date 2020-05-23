0d33cd02557955814a5d1472ed8610a5eee9def4
GaoShuai <gshzyfx@163.com>
2020-05-23 18:36:26 +0800

    修改metadata参数

--- lib/src/source_params_meta.dart
+++ lib/src/source_params_meta.dart
@@ -1,18 +1,24 @@
 class SourceParamsMeta {
 
   String orderNo;
+  String sourceId;
+  String clientSecret;
 
-  SourceParamsMeta({ this.orderNo});
+  SourceParamsMeta({ this.orderNo = "", this.sourceId = "", this.clientSecret = ""});
 
   factory SourceParamsMeta.fromJson(Map<dynamic, dynamic> json) {
     return SourceParamsMeta(
       orderNo: json['orderNo'],
+      sourceId: json['sourceId'],
+      clientSecret: json['clientSecret'],
     );
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     if (this.orderNo != null) data['orderNo'] = this.orderNo;
+    if (this.sourceId != null) data['sourceId'] = this.sourceId;
+    if (this.clientSecret != null) data['clientSecret'] = this.clientSecret;
 
     return data;
   }
