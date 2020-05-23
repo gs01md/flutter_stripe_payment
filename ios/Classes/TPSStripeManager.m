fd3065011cdc2146d85a7c4fb5bb99ea95381900
GaoShuai <gshzyfx@163.com>
2020-05-23 18:32:05 +0800

    修改获取参数的方式

--- ios/Classes/TPSStripeManager.m
+++ ios/Classes/TPSStripeManager.m
@@ -835,41 +835,22 @@ void initializeTPSPaymentNetworksWithConditionalMappings() {
     }
     requestIsCompleted = NO;
 
-    NSString *sourceType = params[@"type"];
-    STPSourceParams *sourceParams;
-    if ([sourceType isEqualToString:@"bancontact"]) {
-        sourceParams = [STPSourceParams bancontactParamsWithAmount:[[params objectForKey:@"amount"] unsignedIntegerValue] name:params[@"name"] returnURL:params[@"returnURL"] statementDescriptor:params[@"statementDescriptor"]];
-    }
-    if ([sourceType isEqualToString:@"giropay"]) {
-        sourceParams = [STPSourceParams giropayParamsWithAmount:[[params objectForKey:@"amount"] unsignedIntegerValue] name:params[@"name"] returnURL:params[@"returnURL"] statementDescriptor:params[@"statementDescriptor"]];
-    }
-    if ([sourceType isEqualToString:@"ideal"]) {
-        sourceParams = [STPSourceParams idealParamsWithAmount:[[params objectForKey:@"amount"] unsignedIntegerValue] name:params[@"name"] returnURL:params[@"returnURL"] statementDescriptor:params[@"statementDescriptor"] bank:params[@"bank"]];
-    }
-    if ([sourceType isEqualToString:@"sepaDebit"]) {
-        sourceParams = [STPSourceParams sepaDebitParamsWithName:params[@"name"] iban:params[@"iban"] addressLine1:params[@"addressLine1"] city:params[@"city"] postalCode:params[@"postalCode"] country:params[@"country"]];
-    }
-    if ([sourceType isEqualToString:@"sofort"]) {
-        sourceParams = [STPSourceParams sofortParamsWithAmount:[[params objectForKey:@"amount"] unsignedIntegerValue] returnURL:params[@"returnURL"] country:params[@"country"] statementDescriptor:params[@"statementDescriptor"]];
-    }
-    if ([sourceType isEqualToString:@"threeDSecure"]) {
-        sourceParams = [STPSourceParams threeDSecureParamsWithAmount:[[params objectForKey:@"amount"] unsignedIntegerValue] currency:params[@"currency"] returnURL:params[@"returnURL"] card:params[@"card"]];
-    }
-    if ([sourceType isEqualToString:@"alipay"]) {
-        sourceParams = [STPSourceParams alipayParamsWithAmount:[[params objectForKey:@"amount"] unsignedIntegerValue] currency:params[@"currency"] returnURL:params[@"returnURL"]];
+    NSDictionary *dict = params[@"metadata"];
+
+    NSString * sourceId = dict[@"sourceId"];
+    NSString * clientSecret = dict[@"clientSecret"];
+
+    if (!sourceId) {
+        sourceId = @"";
     }
 
-    if ([sourceType isEqualToString:@"wechat"]) {
-        sourceParams = [STPSourceParams wechatPayParamsWithAmount:[[params objectForKey:@"amount"] unsignedIntegerValue] currency:params[@"currency"] appId:@"lingoaceparentapp" statementDescriptor:params[@"statementDescriptor"]];
-    }
-
-    if ([sourceType isEqualToString:@"card"]) {
-        sourceParams = [STPSourceParams cardParamsWithCard:[self extractCardParamsFromDictionary:params]];
+    if(!clientSecret){
+        clientSecret = @"";
     }
 
     STPAPIClient* stripeAPIClient = [self newAPIClient];
 
-    [stripeAPIClient retrieveSourceWithId:params[@"sourceId"] clientSecret:params[@"secret"] completion:^(STPSource *source, NSError *error) {
+    [stripeAPIClient retrieveSourceWithId:sourceId clientSecret:clientSecret completion:^(STPSource *source, NSError *error) {
         self->requestIsCompleted = YES;
 
         if (error) {
